a0a88e633cfe753c79c912d220862ada88954c43
Vladyslav Sokolenko <vsokolenko1@gmail.com>
2025-06-18 13:19:15 +0300

    404 page & handler exception

--- src/public/index.php
+++ src/public/index.php
@@ -8,12 +8,23 @@ use App\Controllers\HomeController;
 use App\Controllers\TransactionsController;
 
 define('VIEW_PATH', __DIR__ . '/../app/views');
-  
+
+try {
+    
 $route = new App\Route();
 
 $route  ->get('/', [HomeController::class, 'index'])
         ->get('/transactions', [TransactionsController::class, 'index'])
         ->get('/transactions/create', [TransactionsController::class, 'create'])
+        ->get('/transactions/upload', [TransactionsController::class, 'upload'])
         ->post('/transactions/create', [TransactionsController::class, 'store']);
 
 echo $route->resolve($_SERVER['REQUEST_URI'], strtolower($_SERVER['REQUEST_METHOD']));
+
+} catch (\App\Exceptions\RouteNotFoundException $e) {
+    
+    http_response_code(404);//  same -  header('HTTP/1.1 404 Not Found');
+    
+    echo App\View::make('error/404', [], false);
+    
+}
